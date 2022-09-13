216ae09de2f14f9c277257de6374638ef8215393
emontes <emontes@turista.com.mx>
2022-09-12 21:20:48 -0500

    included hero in top heading

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { StaticImage, GatsbyImage, getImage } from 'gatsby-plugin-image'
 import styled from 'styled-components'
 import { Trans, Link } from 'gatsby-plugin-react-i18next'
 
@@ -7,29 +7,17 @@ const AboutHeader = ({ bgColor, image, main, sub, btnLink, btnText }) => {
   return (
     <Wrapper style={{ backgroundColor: `${bgColor || 'var(--clr-white)'}` }}>
       <div className="hero">
-        <GatsbyImage
-          image={getImage(image)}
-          alt="Programador Gatsby"
+        <StaticImage
           className="hero-img"
-          placeholder="tracedSVG"
-          layout="constrained"
+          alt="Marcos"
+          src="../assets/images/money-1.jpg"
         />
-        <div className="hero-container">
+        <div className="hero-container font-yrsa text-slate-100 text-center">
           <div className="header__text-box">
-            <h1 className="heading-primary">
-              <span className="heading-primary--main">
-                {main || 'Programador Front-end'}
-              </span>
-              <span className="heading-primary--sub">
-                {sub || 'en Cancún desde 2002, programando desde 1984'}
-              </span>
-            </h1>
-
-            {btnLink && (
-              <Link to={`/${btnLink}`} className="btn btn--white btn--animated">
-                <Trans>{btnText || 'contact'}</Trans>
-              </Link>
-            )}
+            <div className="text-4xl md:text-6xl">
+              {main || 'Marcos Iván Quintana'}
+            </div>
+            <div className="text-xl">{sub || 'Money is not enough'}</div>
           </div>
         </div>
       </div>
@@ -45,12 +33,10 @@ const Wrapper = styled.header`
     border-radius: var(--borderRadius);
   }
   .hero {
-    height: 89vh;
+    height: 20vh;
     background-color: var(--clr-primary-9);
     background-size: cover;
     background-position: top;
-    -webkit-clip-path: polygon(0 0, 100% 0, 100% 75vh, 0 100%);
-    clip-path: polygon(0 0, 100% 0, 100% 75vh, 0 100%);
     position: relative;
   }
   .hero-container {
@@ -62,47 +48,12 @@ const Wrapper = styled.header`
     display: flex;
     align-items: center;
     justify-content: center;
+    text-shadow: 1px 1px 2px black;
 
     background: linear-gradient(
       to bottom right,
       rgba(0, 37, 92, 0.7),
       rgba(199, 226, 222, 0.9)
     );
-    border-radius: var(--borderRadius);
-  }
-
-  .header__text-box {
-    position: absolute;
-    top: 42%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
-  }
-  .heading-primary {
-    color: var(--clr-primary-10);
-    text-shadow: 1px 1px 2px black;
-    text-transform: uppercase;
-    backface-visibility: hidden;
-    margin-bottom: 6rem;
-    &--main {
-      display: block;
-      font-size: 3.2rem;
-      font-weight: 400;
-      letter-spacing: 0.5rem;
-      animation-name: moveInLeft;
-      animation-duration: 1s;
-      animation-timing-function: ease-out;
-
-      /* animation-iteration-count: 3;
-      animation-delay: 3s; */
-    }
-    &--sub {
-      margin-top: 2rem;
-      display: block;
-      font-size: 1.4rem;
-      font-weight: 700;
-      letter-spacing: 0.4rem;
-      animation: moveInRight 1s ease-out;
-    }
   }
 `
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,52 +2,24 @@ import React, { useState } from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
 import Navbar from './Navbar'
 import Sidebar from './Sidebar'
+import Hero from './Hero'
 
 const Layout = ({ children }) => {
-  const linkStyle = `
-     hover:opacity-70 bg-red-300;
-     text-sm
-     sm:text-lg
-     transition delay-150
-    `
-  const logoStyle = `
-    text-white 
-    font-rammetto
-    sm:text-base
-    text-sm
-    `
-
   const [isOpen, setIsOpen] = useState(false)
   const toggleSidebar = () => {
     setIsOpen(!isOpen)
   }
   return (
     <div>
-      <div className="flex justify-center items-center flex-col">
-        <StaticImage
-          className="border mb-2 mt-5"
-          src="../assets/images/money-1.jpg"
-          height={150}
-          alt="asml"
-        />
-        <div className="text-center">
-          <p className="font-yrsa text-3xl md:text-5xl">Marcos Ivan Quintana</p>
-          <p className="font-yrsa text-gray-400 text-base md:text-lg">
-            - Money is not enought -
-          </p>
-        </div>
-      </div>
-
+      <Hero />
       <Navbar toggleSidebar={toggleSidebar} />
 
       <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
 
-      <main>{children}</main>
+      {children}
 
       <footer className="text-center py-8 bg-cyan-600 text-gray-100">
-        <p>
-          Copyright 2030 <span className={logoStyle}>Test Blog</span>
-        </p>
+        <p>Luego vamos por el footer</p>
       </footer>
     </div>
   )
